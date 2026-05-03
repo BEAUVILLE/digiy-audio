bb757ac143bb640e9ae852e83a7f5c647c197236
Jean Baptiste Beauville <you@example.com>
2026-05-03 22:54:02 +0000

    Updates

--- assets/index-Ng2gqIFm.js
+++ assets/index-Ng2gqIFm.js
@@ -87,7 +87,7 @@ Error generating stack: `+u.message+`
  *
  * This source code is licensed under the ISC license.
  * See the LICENSE file in the root directory of this source tree.
- */const Bd=cn("Zap",[["path",{d:"M4 14a1 1 0 0 1-.78-1.63l9.9-10.2a.5.5 0 0 1 .86.46l-1.92 6.02A1 1 0 0 0 13 10h7a1 1 0 0 1 .78 1.63l-9.9 10.2a.5.5 0 0 1-.86-.46l1.92-6.02A1 1 0 0 0 11 14z",key:"1xq2db"}]]),Wd={fr:"Français",en:"English",wo:"Wolof","fr-di":"Diola","fr-man":"Mandingue"},Rn={id:"digiy-audio-premiere-ecoute",original:`Bienvenue dans DIGIY AUDIO.
+ */const Bd=cn("Zap",[["path",{d:"M4 14a1 1 0 0 1-.78-1.63l9.9-10.2a.5.5 0 0 1 .86.46l-1.92 6.02A1 1 0 0 0 13 10h7a1 1 0 0 1 .78 1.63l-9.9 10.2a.5.5 0 0 1-.86-.46l1.92-6.02A1 1 0 0 0 11 14z",key:"1xq2db"}]]),Wd={fr:"Français",en:"Anglais"},Rn={id:"digiy-audio-premiere-ecoute",original:`Bienvenue dans DIGIY AUDIO.
 
 Ici, tu peux coller un texte, un article, une note, une idée ou un message important, puis le garder pour l’écouter plus tard.
 
